ad4deca6b31f86069d39c108569d4d2012e62413
XenonIsAwesome <ofek171202@gmail.com>
2025-05-28 22:31:30 +0300

    removed build folder

--- answers/build/CMakeFiles/server.dir/src/utils/general_utils.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/server.dir/src/utils/general_utils.cpp.o: \
- /home/xenon/projects/docker-workshop/answers/src/utils/general_utils.cpp \
- /usr/include/stdc-predef.h \
- /home/xenon/projects/docker-workshop/answers/src/utils/general_utils.h
